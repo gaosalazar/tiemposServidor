1b46c2f880bf8a6877bd17068d7e6793b82b6138
gao.salazar <gao.salazar@yavirac.edu.ec>
2018-11-20 22:19:45 -0500

    configuración bd

---
+++ api/controllers/PersonaController.js
@@ -0,0 +1,93 @@
+/**
+ * PersonaController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+  /**
+   * `PersonaController.id()`
+   */
+  id: async function (req, res) {
+    return res.json({
+      todo: 'id() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.nombre()`
+   */
+  nombre: async function (req, res) {
+    return res.json({
+      todo: 'nombre() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.apellido()`
+   */
+  apellido: async function (req, res) {
+    return res.json({
+      todo: 'apellido() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.identificacion()`
+   */
+  identificacion: async function (req, res) {
+    return res.json({
+      todo: 'identificacion() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.correo()`
+   */
+  correo: async function (req, res) {
+    return res.json({
+      todo: 'correo() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.direccion()`
+   */
+  direccion: async function (req, res) {
+    return res.json({
+      todo: 'direccion() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.telefono()`
+   */
+  telefono: async function (req, res) {
+    return res.json({
+      todo: 'telefono() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.es_cliente()`
+   */
+  es_cliente: async function (req, res) {
+    return res.json({
+      todo: 'es_cliente() is not implemented yet!'
+    });
+  },
+
+  /**
+   * `PersonaController.es_empleado()`
+   */
+  es_empleado: async function (req, res) {
+    return res.json({
+      todo: 'es_empleado() is not implemented yet!'
+    });
+  }
+
+};
+
---
+++ api/models/Persona.js
@@ -0,0 +1,48 @@
+/**
+ * Persona.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+
+    //  ╔═╗╦═╗╦╔╦╗╦╔╦╗╦╦  ╦╔═╗╔═╗
+    //  ╠═╝╠╦╝║║║║║ ║ ║╚╗╔╝║╣ ╚═╗
+    //  ╩  ╩╚═╩╩ ╩╩ ╩ ╩ ╚╝ ╚═╝╚═╝
+
+    id: { type: 'number', autoIncrement: true },
+
+    nombre: { type: 'string' },
+
+    apellido: { type: 'string' },
+
+    identificacion: { type: 'string' },
+
+    correo: { type: 'string' },
+
+    direccion: { type: 'string' },
+
+    telefono: { type: 'string' },
+
+    es_cliente: { type: 'string' },
+
+    es_empleado: { type: 'string' }
+
+    //  ╔═╗╔╦╗╔╗ ╔═╗╔╦╗╔═╗
+    //  ║╣ ║║║╠╩╗║╣  ║║╚═╗
+    //  ╚═╝╩ ╩╚═╝╚═╝═╩╝╚═╝
+
+
+    //  ╔═╗╔═╗╔═╗╔═╗╔═╗╦╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+    //  ╠═╣╚═╗╚═╗║ ║║  ║╠═╣ ║ ║║ ║║║║╚═╗
+    //  ╩ ╩╚═╝╚═╝╚═╝╚═╝╩╩ ╩ ╩ ╩╚═╝╝╚╝╚═╝
+
+  },
+
+  primarykey: 'id'
+
+};
+
